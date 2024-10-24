8d3296a0d3886fead71525f1c9100c2522b5718e
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-24 13:09:22 +0300

    Fixed token service v2

--- Apps.Confluence/Constants/CredNames.cs
+++ Apps.Confluence/Constants/CredNames.cs
@@ -2,13 +2,13 @@ namespace Apps.Confluence.Constants;
 
 public static class CredNames
 {
-    public const string AccessToken = "access_token";
+    public const string AccessToken = "accessToken";
     
-    public const string RefreshToken = "refresh_token";
+    public const string RefreshToken = "refreshToken";
     
-    public const string ExpiresIn = "expires_at";
+    public const string ExpiresIn = "expiresAt";
     
-    public const string CreatedAt = "created_at";
+    public const string CreatedAt = "createdAt";
     
-    public const string ConfluenceId = "confluence_id";
+    public const string ConfluenceId = "confluenceId";
 }
