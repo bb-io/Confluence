69e7a9956fe5e2f5524070b9d2c70fce6c7cead5
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-24 11:35:38 +0300

    Fixed application class

--- Apps.Confluence/Application.cs
+++ Apps.Confluence/Application.cs
@@ -1,17 +1,48 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Confluence.Auth.OAuth2;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Confluence;
 
-public class Application : IApplication
+public class Application : BaseInvocable, IApplication, ICategoryProvider
 {
+    private readonly Dictionary<Type, object> _typesInstances;
+    
+    public Application(InvocationContext invocationContext) : base(invocationContext)
+    {
+        _typesInstances = CreateTypesInstances();
+    }
+
     public string Name
     {
-        get => "App";
+        get => "Confluence";
         set { }
     }
 
     public T GetInstance<T>()
     {
-        throw new NotImplementedException();
+        if (!_typesInstances.TryGetValue(typeof(T), out var value))
+        {
+            throw new InvalidOperationException($"Instance of type '{typeof(T)}' not found");
+        }
+        
+        return (T)value;
+    }
+
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => new [] { ApplicationCategory.Cms };
+        set { }
+    }
+    
+    private Dictionary<Type, object> CreateTypesInstances()
+    {
+        return new Dictionary<Type, object>
+        {
+            { typeof(IOAuth2AuthorizeService), new OAuth2AuthorizeService(InvocationContext) },
+            { typeof(IOAuth2TokenService), new OAuth2TokenService(InvocationContext) }
+        };
     }
 }
