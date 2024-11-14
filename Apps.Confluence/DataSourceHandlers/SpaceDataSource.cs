7217726b6faeb304df4b8b9b1e6258f55e269b66
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-14 17:17:29 +0200

    Minor improvements

--- Apps.Confluence/DataSourceHandlers/SpaceDataSource.cs
+++ Apps.Confluence/DataSourceHandlers/SpaceDataSource.cs
@@ -11,11 +11,38 @@ public class SpaceDataSource(InvocationContext invocationContext) : AppInvocable
 {
     public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
-        var request = new ApiRequest("/api/space", Method.Get, Creds);
-        var spacesResponse = await Client.ExecuteWithErrorHandling<SearchSpaceResponse>(request);
-        
-        return spacesResponse.Results
-            .Where(x => context.SearchString == null || x.Name.Contains(context.SearchString))
-            .ToDictionary(x => x.Id, x => x.Name);
+        var allResults = new Dictionary<string, string>();
+        var start = 0;
+        var limit = 25;
+
+        while (true)
+        {
+            var request = new ApiRequest("/api/space", Method.Get, Creds);
+            request.AddParameter("start", start, ParameterType.QueryString);
+            request.AddParameter("limit", limit, ParameterType.QueryString);
+
+            var spacesResponse = await Client.ExecuteWithErrorHandling<SearchSpaceResponse>(request);
+
+            if (spacesResponse.Results != null! && spacesResponse.Results.Any())
+            {
+                var filteredResults = spacesResponse.Results
+                    .Where(x => context.SearchString == null || x.Name.Contains(context.SearchString))
+                    .ToDictionary(x => x.Id, x => x.Name);
+
+                foreach (var kvp in filteredResults)
+                {
+                    allResults[kvp.Key] = kvp.Value;
+                }
+            }
+
+            if (spacesResponse.Size < limit)
+            {
+                break;
+            }
+
+            start += limit;
+        }
+
+        return allResults;
     }
 }
---
+++ Apps.Confluence/Models/Requests/Content/UpdateContentRequest.cs
@@ -0,0 +1,14 @@
+﻿using Apps.Confluence.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.Confluence.Models.Requests.Content;
+
+public class UpdateContentRequest
+{
+    public FileReference File { get; set; } = default!;
+
+    [Display("Content ID"), DataSource(typeof(ContentDataSource))]
+    public string? ContentId { get; set; }
+}
