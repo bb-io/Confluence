b607b30bcfa971f9d27fc58ceaaae3d615c90fe0
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-07-30 13:28:22 +0300

    scopes changes

--- Apps.Confluence/ApplicationConstants.cs
+++ Apps.Confluence/ApplicationConstants.cs
@@ -6,5 +6,5 @@ public class ApplicationConstants
     
     public static string ClientSecret => "#{CONFLUENCE_CLIENT_SECRET}#"; 
     
-    public static string Scope => "#{CONFLUENCE_SCOPE}#";
+    public static string Scope => "write:confluence-content read:confluence-space.summary write:confluence-space write:confluence-file read:confluence-props write:confluence-props manage:confluence-configuration read:confluence-content.all search:confluence read:confluence-content.summary read:confluence-content.permission read:confluence-user read:confluence-groups write:confluence-groups readonly:content.attachment:confluence read:content-details:confluence read:content:confluence write:content:confluence delete:content:confluence read:attachment:confluence read:custom-content:confluence write:custom-content:confluence delete:custom-content:confluence read:template:confluence write:template:confluence read:content.permission:confluence read:content.property:confluence write:content.property:confluence read:space:confluence read:space-details:confluence offline_access";
 }
