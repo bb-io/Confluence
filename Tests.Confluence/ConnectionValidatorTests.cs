00fd58ab5b84539d3d27706f4a569398636f75db
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-24 10:28:43 +0200

    update conn validator & its tests

--- Tests.Confluence/ConnectionValidatorTests.cs
+++ Tests.Confluence/ConnectionValidatorTests.cs
@@ -7,30 +7,24 @@ namespace Tests.Confluence;
 [TestClass]
 public class ConnectionValidatorTests : TestBase
 {
-    private ConnectionValidator _validator;
-
-    [TestInitialize]
-    public void Setup()
-    {
-        _validator = new ConnectionValidator();
-    }
+    private readonly ConnectionValidator _validator = new();
 
     [TestMethod]
     public async Task ValidateConnection_WithValidCredentials_ReturnsValid()
     {
         var result = await _validator.ValidateConnection(Creds, CancellationToken.None);
 
+        Console.WriteLine(result.Message);
         Assert.IsTrue(result.IsValid);
     }
 
     [TestMethod]
     public async Task ValidateConnection_WithInvalidCredentials_ReturnsInvalid()
     {
-        var invalidCreds = Creds.Select(x =>
-            new AuthenticationCredentialsProvider(AuthenticationCredentialsRequestLocation.None, x.KeyName, x.Value + "_incorrect"));
-
+        var invalidCreds = Creds.Select(x => new AuthenticationCredentialsProvider(x.KeyName, x.Value + "_incorrect"));
         var result = await _validator.ValidateConnection(invalidCreds, CancellationToken.None);
 
+        Console.WriteLine(result.Message);
         Assert.IsFalse(result.IsValid);
     }
 }
